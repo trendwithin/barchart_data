0c382e7f7c38e30a2b951d530ddb8fda1ff7a5d1
Michael Becco <e4e6d4d5@gmail.com>
2015-06-29 10:45:09 -0700

    Add requires to test_helper

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'barchart_data'
+require 'barchart_data/alltimehigh'
+require 'mechanize'
+require 'nokogiri'
+require 'fakeweb'
 
 require 'minitest/autorun'
