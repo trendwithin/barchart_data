e0ebaff3f70fcb6fb3de9245c3bae83a693d1045
Michael Becco <e4e6d4d5@gmail.com>
2015-06-30 10:22:28 -0700

    Add Require File

---
+++ lib/barchart_data/alltimehigh.rb
@@ -0,0 +1,30 @@
+require 'barchart_data'
+require 'active_record'
+require 'mechanize'
+
+module BarchartData
+  class AllTimeHigh
+    attr_reader :url, :agent
+
+    def initialize
+      @url = "http://www.barchart.com/stocks/athigh.php?_dtp1-0"
+      @agent = Mechanize.new
+    end
+
+    def parse_url
+      @agent.get(@url).search("input")
+    end
+
+    def strip_symbols(page)
+      symbols = page[6].to_s
+      strip_symbols = symbols.scan(/[A-Z]+,[^a-z]+[A-Z]/)
+      tickers = strip_symbols[0].spli(',')
+    end
+
+    def insert_symbols(symbols)
+      symbols.each do |s|
+        AllTimeHigh.new(symbol: s, saved_on: Time.now.to_date.to_s)
+      end
+    end
+  end
+end
