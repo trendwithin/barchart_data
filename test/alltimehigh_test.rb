eba3413d8cee4aa34477aa7b643ffb650f52b9e5
Michael Becco <e4e6d4d5@gmail.com>
2015-06-30 10:24:41 -0700

    Add Test for AllTimeHigh

--- test/alltimehigh_test.rb
+++ test/alltimehigh_test.rb
@@ -1,9 +1,11 @@
 require './test/minitest_helper'
+require 'fakeweb'
 
 class TestAllTimeHigh < Minitest::Test
   def setup
     @ath = ::BarchartData::AllTimeHigh.new
     @stream = "./test/test_files/athigh.php"
+    @entry = AllTimeHigh.first
   end
 
   def test_it_should_be_valid
@@ -11,7 +13,6 @@ class TestAllTimeHigh < Minitest::Test
   end
 
   def test_url
-    skip
     assert_equal 'http://www.barchart.com/stocks/athigh.php?_dtp1-0', @ath.url
   end
 
@@ -25,7 +26,7 @@ class TestAllTimeHigh < Minitest::Test
    assert_instance_of Nokogiri::XML::NodeSet, noko_node
   end
 
-  def test_symbols_include_expected_values
+  def test_nodeset_symbols_include_expected_values
     FakeWeb.register_uri(:get, @ath.url, :body => @stream, :content_type => 'text/html')
     noko_node = noko_node = @ath.agent.get(@ath.url).search("input")[6].to_s
     strip_tickers = noko_node.scan(/[A-Z]+,[^a-z]+[A-Z]/)
@@ -35,6 +36,33 @@ class TestAllTimeHigh < Minitest::Test
     expected.each do |e|
       assert_includes symbols, e
     end
+  end
+
+  def test_current_data_in_model
+    assert_equal AllTimeHigh.first.symbol, 'ABC'
+    assert_equal AllTimeHigh.first.saved_on, Time.now.to_date.to_s
+  end
+
+  def test_data_model_validation_for_symbol
+    @entry.symbol = '    '
+    refute @entry.valid?
+  end
+
+  def test_data_model_validation_for_saved_on
+    @entry.saved_on = '         '
+    refute @entry.valid?
+  end
+
+  def test_db_insertion
+    FakeWeb.register_uri(:get, @ath.url, :body => @stream, :content_type => 'text/html')
+    noko_node = noko_node = @ath.agent.get(@ath.url).search("input")[6].to_s
+    strip_tickers = noko_node.scan(/[A-Z]+,[^a-z]+[A-Z]/)
+    symbols = strip_tickers[0].split(',')
+
+    symbols.each do |s|
+      AllTimeHigh.create(symbol: s, saved_on: Time.now.to_date.to_s)
+    end
+    assert AllTimeHigh.find_by(symbol: 'LOPE'), !nil
   end
 end
 
