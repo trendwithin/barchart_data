f5a23200890fcffd99cfd55a526c902a3efb08ea
Michael Becco <e4e6d4d5@gmail.com>
2015-07-15 09:56:32 -0700

    modify generator

--- lib/generators/barchart_data/install/install_generator.rb
+++ lib/generators/barchart_data/install/install_generator.rb
@@ -5,7 +5,7 @@ require 'rails/generators/active_record'
 
 module BarchartData
   module Generators
-    class BarchartDataGenerator < Rails::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('..', __FILE__)
 
       def copy_schema
