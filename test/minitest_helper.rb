22e323a6c5b171e63437509feab7ee8371dc121d
Michael Becco <e4e6d4d5@gmail.com>
2015-09-09 20:38:29 -0700

    Add require for newhigh

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -2,6 +2,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'barchart_data'
 require 'minitest/autorun'
 require 'barchart_data/alltimehigh'
+require 'barchart_data/newhigh'
+
 ActiveRecord::Base.establish_connection(:adapter => "sqlite3",
                                        :database => File.dirname(__FILE__) + "/barchart.sqlite3")
 
