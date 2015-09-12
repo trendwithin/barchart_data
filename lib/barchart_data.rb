8f22115df8c20902e73f83a9adfe0d84ff96fe1d
Michael Becco <e4e6d4d5@gmail.com>
2015-09-11 19:26:03 -0700

    Add require for high_low_scraper

--- lib/barchart_data.rb
+++ lib/barchart_data.rb
@@ -2,6 +2,7 @@ require "barchart_data/version"
 require 'barchart_data/alltimehigh'
 require 'barchart_data/newhigh'
 require 'barchart_data/scraper'
+require 'barchart_data/high_low_scraper'
 module BarchartData
   # Your code goes here...
 end
