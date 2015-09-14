7a3bdbe087ddaaf224ceb96817e631319e0c979f
Michael Becco <e4e6d4d5@gmail.com>
2015-09-14 10:39:48 -0700

    Modify method definitions to more clearly state intention
    ADD migration for alltimelow, highlow

--- lib/generators/barchart_data/install/install_generator.rb
+++ lib/generators/barchart_data/install/install_generator.rb
@@ -8,12 +8,18 @@ module BarchartData
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../templates', __FILE__)
 
-      def copy_schema
+      def copy_migrations
         time = Time.now.strftime("%Y%m%d%H%M%S")
-        template "schema.rb", "db/migrate/#{time}_create_all_time_highs.rb"
+        template "alltimehigh_migration.rb", "db/migrate/#{time}_create_all_time_highs.rb"
         template 'newhigh_migration.rb', "db/migrate/#{time}_create_new_highs.rb"
-        copy_file "../../../../barchart_data/alltimehigh.rb", 'lib/barchart_data/alltimehigh.rb'
-        copy_file "../../../../barchart_data/newhigh.rb", 'lib/barchart_data/newhigh.rb'
+        template 'newlow_migration.rb', "db/migrate/#{time}_create_new_lows.rb"
+        template 'alltimelow_migration.rb', "db/migrate/#{time}_create_all_time_lows.rb"
+        template 'highlow_migration.rb', "db/migrate/#{time}_create_high_lows.rb"
+      end
+
+      def copy_classes
+        copy_file "../../../../barchart_data/scraper.rb", 'lib/barchart_data/scraper.rb'
+        copy_file "../../../../barchart_data/high_low_scraper.rb", 'lib/barchart_data/high_low_scraper.rb'
         copy_file "../../../../../bin/barchart_data", 'bin/barchart'
       end
     end
