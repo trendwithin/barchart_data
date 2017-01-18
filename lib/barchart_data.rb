dcf0153c5eb111f148e6df6184da1008b653785b
Michael Becco <e4e6d4d5@gmail.com>
2017-01-18 15:39:14 -0800

    Test NewHighs Data Extraction

--- lib/barchart_data.rb
+++ lib/barchart_data.rb
@@ -5,6 +5,8 @@ require "barchart_data/version"
 # require 'barchart_data/high_low_scraper'
 require 'barchart_scripts/barchart_connection'
 require 'barchart_scripts/scrape_new_highs_new_lows'
+require 'barchart_scripts/scrape_new_highs'
+
 module BarchartData
   # Your code goes here...
 end
